1f0aab6716d201c4098fe6e97e09d3a432ccaf46
Connor Slade <connor@connorcode.com>
2021-07-16 01:07:47 -0400

    🔗 Normal Queue

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const fetch = require("node-fetch");
 
 /**
+ * Fetch Current Queue information from https://www.2b2t.io/
+ * @async
+ * @returns {Promise<Array>} queue - Players in Queue.
+ */
+async function queue() {
+  let queue = await fetch("https://www.2b2t.io/api/queue?last=true");
+  queue = await queue.json();
+
+  return {queue: queue[1], updatetime: queue[0]};
+}
+
+/**
  * Fetch the Priority Queue information from https://api.2b2t.dev/prioq
+ * @async
  * @returns {Promise<Array>} prio[1] - Players in Prio queue. prio[2] - Estimated Time in Prio queue.
  */
 async function prioQ() {
@@ -11,4 +24,4 @@ async function prioQ() {
   return prioQ;
 }
 
-module.exports = { prioQ };
+module.exports = { prioQ, queue };
