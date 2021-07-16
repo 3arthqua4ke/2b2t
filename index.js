22c08a99a9f875975adc80012d1a80c7286776e2
Connor Slade <connor@connorcode.com>
2021-07-16 00:56:03 -0400

    🧹 Cleanup Prio queue functions

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
+/**
+ * Fetch the Priority Queue information from https://api.2b2t.dev/prioq
+ * @returns {Promise<Array>} prio[1] - Players in Prio queue. prio[2] - Estimated Time in Prio queue.
+ */
+async function prioQ() {
+  let prioQ = await fetch("https://api.2b2t.dev/prioq");
+  prioQ = await prioQ.json();
 
-/*
-
-    Create the prioQ function to fetch the Priority Queue information from https://api.2b2t.dev/prioq. 
-    Resolve the information as JSON format & return the function.
-
-*/
-
-let prioQ = async (message) => {
-
-    let PrioQ = await fetch('https://api.2b2t.dev/prioq');
-    PrioQ = await PrioQ.json();
-
-    return PrioQ;
-
+  return prioQ;
 }
 
-module.exports = prioQ;
-
+module.exports = { prioQ };
